74a667be974547117be3e2beb3e95a0586704316
NoahCummings <349788729@gapps.yrdsb.ca>
2026-01-08 17:53:06 -0500

    Update User.java
    -

--- src/User.java
+++ src/User.java
@@ -11,12 +11,20 @@ public class User {
     //LOGIN INFORMATION
     private String email; //User's email
     private String password; //User's password
-    private boolean admin = false; //Boolean for whether or not the user is an admin (false by default)
     
     //PAYMENT INFORMATION
     private String cardNumber; //User's debit/credit card number (both verified with the Luhn algorithm)
     private String securityCode; //Those 3 funny numbers on the back of the user's card
     private int points; //Integer for the number of points the user has
+    
+    //CONSTRUCTOR METHOD
+    public User(String email, String password, String cardNumber, String securityCode) {
+        this.email = email;
+        this.password = password;
+        this.cardNumber = cardNumber;
+        this.securityCode = securityCode;
+        this.points = 0; //By default, the users start with no points
+    }
     
     public String getEmail() {
         return email;
@@ -24,10 +32,6 @@ public class User {
     
     public String getPassword() {
         return password;
-    }
-    
-    public boolean isAdmin() {
-        return admin;
     }
     
     public String getCardNumber() {
@@ -42,5 +46,27 @@ public class User {
         return points;
     }
     
-    //TODO: ADD SETTER METHODS
+    public void setEmail(String input) {
+        email = input;
+    }
+    
+    public void setPassword(String input) {
+        password = input;
+    }
+    
+    public void setCardNumber(String input) {
+        cardNumber = input;
+    }
+    
+    public void setSecurityCode(String input) {
+        securityCode = input;
+    }
+    
+    public void addPoints(int points) { //Safer to split this into add and remove
+        this.points += points;
+    }
+    
+    public void lowerPoints(int cost) {
+        this.points -= cost;
+    }
 }
