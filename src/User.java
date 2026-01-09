18f543656460ba4161c0e8161529400656317b1f
NoahCummings <349788729@gapps.yrdsb.ca>
2026-01-09 12:39:53 -0500

    Update User.java
    - User is now the superclass of the admin and customer class

--- src/User.java
+++ src/User.java
@@ -9,64 +9,32 @@
  */
 public class User {
     //LOGIN INFORMATION
-    private String email; //User's email
-    private String password; //User's password
-    
-    //PAYMENT INFORMATION
-    private String cardNumber; //User's debit/credit card number (both verified with the Luhn algorithm)
-    private String securityCode; //Those 3 funny numbers on the back of the user's card
-    private int points; //Integer for the number of points the user has
+    protected String email; //User's email
+    protected String password; //User's password
+    protected boolean isAdmin;
     
     //CONSTRUCTOR METHOD
-    public User(String email, String password, String cardNumber, String securityCode) {
+    protected User(String email, String password) {
         this.email = email;
         this.password = password;
-        this.cardNumber = cardNumber;
-        this.securityCode = securityCode;
-        this.points = 0; //By default, the users start with no points
+    }
+    protected boolean isAdmin() {
+        return isAdmin;
     }
     
-    public String getEmail() {
+    protected String getEmail() {
         return email;
     }
     
-    public String getPassword() {
+    protected String getPassword() {
         return password;
     }
     
-    public String getCardNumber() {
-        return cardNumber;
-    }
-    
-    public String getSecurityCode() {
-        return securityCode;
-    }
-    
-    public int getPoints() {
-        return points;
-    }
-    
-    public void setEmail(String input) {
+    protected void setEmail(String input) {
         email = input;
     }
     
-    public void setPassword(String input) {
+    protected void setPassword(String input) {
         password = input;
-    }
-    
-    public void setCardNumber(String input) {
-        cardNumber = input;
-    }
-    
-    public void setSecurityCode(String input) {
-        securityCode = input;
-    }
-    
-    public void addPoints(int points) {//Safer to split this into add and remove
-        this.points += points;
-    }
-    
-    public void lowerPoints(int cost) {
-        this.points -= cost;
     }
 }
