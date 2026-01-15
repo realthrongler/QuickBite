a7da2904c9e805a8feb4b2f5e5e34febec585ce1
realthrongler <fjack6677@gmail.com>
2026-01-15 15:37:48 -0500

    Creating card validation method

---
+++ src/validateCard.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author noahc
+ */
+public class validateCard {
+    boolean validateCard(String cardNumber) {
+        boolean cardValid = true;
+        
+        
+        return cardValid;
+    } 
+}
