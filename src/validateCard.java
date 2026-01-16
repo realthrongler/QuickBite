4b5b596aae3326b9b12e15ac984224353577d8fe
realthrongler <fjack6677@gmail.com>
2026-01-16 12:54:23 -0500

    Starting luhn algorithm

--- src/validateCard.java
+++ src/validateCard.java
@@ -9,8 +9,21 @@
  */
 public class validateCard { //Luhn algorithm for card number verification
     boolean validateCard(String cardNumber, String securityCode) {
-        boolean cardValid = true;
-        cardNumber = cardNumber.strip();
+        boolean cardValid;
+        
+        cardNumber = cardNumber.strip(); //Removing spaces
+        
+        String[] splitNumber = cardNumber.split(" "); //Removing spaces in between the numbers
+        cardNumber = String.join("", splitNumber); //Combining it into one string
+        
+        for (int i = 0; i < cardNumber.length(); i++) { //Checking if all the characters of the card number are actually numbers
+            if ((int)cardNumber.charAt(i) < 48 || (int)cardNumber.charAt(i) > 57) {
+                cardValid = false;
+            }
+        }
+        
+        char[] cardArray = cardNumber.toCharArray();
+        
         
         return cardValid;
     } 
