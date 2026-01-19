fe0d29c6ad290d23a30347810026162841a19722
realthrongler <fjack6677@gmail.com>
2026-01-19 18:37:38 -0500

    Update BinarySearch.java
    Updated binary search to be recursive, testing pending

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -9,9 +9,27 @@
  */
 import java.util.ArrayList;
 public class BinarySearch { //Class for binary searching things
-    public boolean BinarySearch(String email, ArrayList<Customer> list) {
+    public boolean search(ArrayList<Customer> list, String email) { //This one searches through the arraylist passed in to the method for the input email
+        return binarySearch(list, email, 0, list.size() - 1);
+    }
+    
+    public boolean binarySearch(ArrayList<Customer> list, String email, int left, int right) {
+        //Base case of left pointer being more than right pointer (entire list checked)
+        if (left > right) {
+            return false;
+        }
         
-        return false;
+        int mid = left + (right - left) / 2;
+        String midEmail = list.get(mid).getEmail();
+        int compare = email.compareToIgnoreCase(email);
+        
+        if (compare == 0) {
+            return true;
+        } else if (compare < 0) {
+            return binarySearch(list, email, left, mid - 1);
+        } else {
+            return binarySearch(list, email, mid + 1, right);
+        }
     }
     public void BinarySearch() {
         //Empty for Ivan to fill out and adjust
