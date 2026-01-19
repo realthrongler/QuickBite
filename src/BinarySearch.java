339fc9bc993e8cc1003555ac0908c13379310d38
realthrongler <fjack6677@gmail.com>
2026-01-19 15:15:21 -0500

    New binary search class

---
+++ src/BinarySearch.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author noahc
+ */
+import java.util.ArrayList;
+public class BinarySearch { //Class for binary searching things
+    public boolean BinarySearch(String email, ArrayList<Customer> list) {
+        
+        return false;
+    }
+    public void BinarySearch() {
+        //Empty for Ivan to fill out and adjust
+    }
+}
